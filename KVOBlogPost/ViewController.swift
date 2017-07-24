519f4b500e57a8d1e2a47cba7ef8f5a69a63c8a9
pattypatpat2632 <oleary.audio@gmail.com>
2017-07-24 16:10:09 -0400

    first commit

---
+++ KVOBlogPost/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  KVOBlogPost
+//
+//  Created by Patrick O'Leary on 7/24/17.
+//  Copyright © 2017 Patrick O'Leary. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
